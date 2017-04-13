6c3563002cfd4de1f5d67314f7e9f06380888829
led <kaspari22@gmail.com>
2017-04-13 12:30:09 +0200

    Namespace upgrade...

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << std::endl;
+
+    for(int i = 0;i<100;i++){
+        cout << i << endl;
+    }
+
     return 0;
 }
